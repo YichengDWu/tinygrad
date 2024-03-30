ddd5a2eaba5cf6908ef7cad4a7d85f04b2f41b93
Ethan Wu <yicheng.wu@ucalgary.ca>
2024-03-29 17:01:30 -0700

    improvements

--- tinygrad/shape/int_tuple.py
+++ tinygrad/shape/int_tuple.py
@@ -1,15 +1,15 @@
 from collections import deque
 import functools
 from typing import Union, Tuple
-from tinygrad.shape.symbolic import Node
+from tinygrad.shape.symbolic import sint, Variable, MulNode, SumNode
 from tinygrad.helpers import prod
 
-def is_tuple(x): return isinstance(x, tuple)
-def is_int(x): return isinstance(x, int) or isinstance(x, Node)
+Integer = sint  # might change
+def is_int(x): return isinstance(x, (int, Variable, MulNode, SumNode))
 
 @functools.lru_cache(maxsize=None)
 def flatten(t):
-  if is_tuple(t):
+  if isinstance(t, tuple):
     if len(t) == 0:
       return t
     else:
@@ -23,14 +23,14 @@ def signum(a): return bool(a > 0) - bool(a < 0)
 
 @functools.lru_cache(maxsize=None)
 def shape_div(a, b):
-  if is_tuple(a):    # tuple,
-    r: deque[Union[int, Node, Tuple]] = deque()
+  if isinstance(a, tuple):    # tuple,
+    r: deque[Union[Integer, Tuple]] = deque()
     for v in reversed(a):
       r.appendleft(shape_div(v,b))
       b = shape_div(b, product(v))
     return tuple(r)
   else:
-    if is_tuple(b):                    # "int" tuple
+    if isinstance(b, tuple):                    # "int" tuple
       return shape_div(a, product(b))
     else:                              # "int" "int"
       assert a % b == 0 or b % a == 0, "shape_div: a and b are not divisible"
@@ -40,19 +40,27 @@ def shape_div(a, b):
         return signum(a*b)
 
 @functools.lru_cache(maxsize=None)
-def compact_strides(a, init=1):
-    if is_tuple(a):
-        if is_tuple(init):  # tuple tuple
-            assert len(a) == len(init)
-            return tuple(compact_strides(v, i) for v, i in zip(a, init))
-        else:  # tuple "int"
-            r: deque[Union[int, Node, Tuple]] = deque()
-            for v in reversed(a):
-                r.appendleft(compact_strides(v, init))
-                init = init * product(v)
-            return tuple(r)
-    else:
-      return init if is_int(init) else RuntimeError("compact_strides: init is not an int")
+def compact_strides(a, init: Union[Tuple, int]=1) -> Union[Tuple, Integer]:
+    if isinstance(a, tuple) and isinstance(init, tuple):
+      assert len(a) == len(init)
+      return tuple(compact_strides(v, i) for v, i in zip(a, init))
+    if isinstance(a, tuple) and isinstance(init, int):
+        r: deque[Union[Integer, Tuple]] = deque()
+        for v in reversed(a):
+            r.appendleft(compact_strides(v, init))
+            init = init * product(v)
+        return tuple(r)
+    if isinstance(init, int): return init
+    raise RuntimeError("compact_strides: init is not an int")
+
+@functools.lru_cache(maxsize=None)
+def canonicalize_strides(shape:Union[Integer, Tuple], strides:Union[Integer, Tuple]) -> Union[Integer, Tuple]:
+  if isinstance(shape, tuple) and isinstance(strides, tuple):
+    return tuple(canonicalize_strides(s, d) for s, d in zip(shape, strides))
+  elif is_int(shape) and is_int(strides):
+    return 0 if shape == 1 else strides
+  else:
+    raise ValueError(f"Invalid combination of input values: {shape=}, {strides=}")
 
 def idx2crd(idx, shape, strides=None):
     if strides is None:
@@ -60,10 +68,10 @@ def idx2crd(idx, shape, strides=None):
 
     if is_int(idx) and is_int(shape) and is_int(strides):
         return (idx // strides) % shape
-    elif is_int(idx) and is_tuple(shape) and is_tuple(strides):
+    elif is_int(idx) and isinstance(shape, tuple) and isinstance(strides, tuple):
       assert len(shape) == len(strides)
       return tuple(idx2crd(idx, s, d) for s,d in zip(shape,strides))
-    elif is_tuple(idx) and is_tuple(shape) and is_tuple(strides):
+    elif isinstance(idx, tuple) and isinstance(shape, tuple) and isinstance(strides, tuple):
         if not (len(idx) == len(shape) == len(strides)):
             raise ValueError("Length of idx, shape, and strides must match for tuple inputs")
         return tuple(idx2crd(i, s, d) for i, s, d in zip(idx, shape, strides))
@@ -71,15 +79,13 @@ def idx2crd(idx, shape, strides=None):
         raise TypeError("Invalid combination of input types")
 
 def crd2idx(crd, shape, strides=None):
-  if strides is None:
-    strides = compact_strides(shape)
+  if strides is None: strides = compact_strides(shape)
 
-  if is_int(crd) and is_int(shape) and is_int(strides):
-    return crd * strides
-  elif is_tuple(crd) and is_tuple(shape) and is_tuple(strides):
+  if is_int(crd) and is_int(shape) and is_int(strides): return crd * strides
+  elif isinstance(crd, tuple) and isinstance(shape, tuple) and isinstance(strides, tuple):
       assert len(crd) == len(shape) and len(crd) == len(strides)
       return sum(crd2idx(c, s, d) for c, s, d in zip(crd, shape, strides))
-  elif is_int(crd) and is_tuple(shape) and is_tuple(strides):
+  elif is_int(crd) and isinstance(shape, tuple) and isinstance(strides, tuple):
     assert len(shape) == len(strides)
     result = 0
     for i in range(len(shape) - 1, 0, -1):
@@ -90,8 +96,7 @@ def crd2idx(crd, shape, strides=None):
     raise ValueError(f"Invalid combination of input values: {crd=}, {shape=}, {strides=}")
 
 def crd2crd(crd, dst_shape, src_shape):
-  if is_tuple(crd) and is_tuple(dst_shape) and is_tuple(src_shape):
-    return tuple(crd2crd(x, y, z) for x, y, z in zip(crd, dst_shape, src_shape))
+  if isinstance(crd, tuple) and isinstance(dst_shape, tuple) and isinstance(src_shape, tuple):
+      return tuple(crd2crd(x, y, z) for x, y, z in zip(crd, dst_shape, src_shape))
   else:
     return idx2crd(crd2idx(crd, src_shape), dst_shape)
-
--- tinygrad/shape/view2.py
+++ tinygrad/shape/view2.py
@@ -1,69 +1,105 @@
-from typing import Tuple, Optional, Union
-from tinygrad.shape.int_tuple import compact_strides, crd2idx, product, shape_div, flatten
-from tinygrad.shape.symbolic import Node, Variable
+from typing import Tuple, Optional, Union, cast
+from tinygrad.shape.int_tuple import Integer, compact_strides, canonicalize_strides, crd2idx, product, shape_div, flatten
+from tinygrad.shape.symbolic import Variable
 import functools
 from collections import deque
+from dataclasses import dataclass
 
+@dataclass(frozen=True)
 class View:
-  shape: Union[Tuple, Node]
-  strides: Union[Tuple, Node]
+  shape: Union[Tuple, Integer]
+  strides: Union[Tuple, Integer]
 
-  def __init__(self, shape, strides=None):
-    self.shape  = shape
-    if strides is None:
-      self.strides = compact_strides(self.shape)
-    else:
-      self.strides = strides
-
-  def __eq__(self, other):
-    return self.shape == other.shape and self.strides == other.strides
-
-  def __len__(self):
+  def __len__(self) -> int:
     return len(self.shape) if isinstance(self.shape, tuple) else 1
 
-  def __call__(self, crd):
+  def __call__(self, crd) -> Integer:
     return crd2idx(crd, self.shape, self.strides)
 
-  def __getitem__(self, i):
+  def __getitem__(self, i) -> 'View':
     if isinstance(self.shape, tuple) and isinstance(self.strides, tuple):
       return View(self.shape[i], self.strides[i])
     else:
       assert i == 0
       return self
 
-  def size(self):
-    return product(self.shape)
+  @functools.lru_cache(maxsize=None)  # pylint: disable=method-cache-max-size-none
+  def size(self) -> int: return product(self.shape)
 
-  def cosize(self):
-    return self(self.size() - 1) + 1
+  @functools.lru_cache(maxsize=None)  # pylint: disable=method-cache-max-size-none
+  def cosize(self) -> int: return self(self.size() - 1) + 1
 
-  def reshape(self, shape):
-    assert product(self.shape) == product(shape), f"Cannot reshape {self.shape} to {shape}"
-    return composition(self, View(shape))
+  @property
+  @functools.lru_cache(maxsize=None)
+  def flat_shape(self):
+    return tuple(product(s) for s in self.shape) if isinstance(self.shape, tuple) else self.shape
 
-  def permute(self, perm: Tuple):
+  @staticmethod
+  @functools.lru_cache(maxsize=None)
+  def create(shape:Union[Integer, Tuple], strides:Optional[Union[Integer, Tuple]]=None):
+    strides = canonicalize_strides(shape, compact_strides(shape)) if strides is None else strides
+    return View(shape, strides)
+
+
+  @functools.lru_cache(maxsize=None)  # pylint: disable=method-cache-max-size-none
+  def permute(self, perm: Tuple) -> 'View':
     assert isinstance(self.shape, tuple) and isinstance(self.strides, tuple)
     assert len(perm) == len(self.shape)
     new_shape = tuple(self.shape[i] for i in perm)
     new_strides = tuple(self.strides[i] for i in perm)
-    return View(new_shape, new_strides)
+    return View.create(new_shape, new_strides)
+
+  @functools.lru_cache(maxsize=None)  # pylint: disable=method-cache-max-size-none
+  def expand(self, new_shape: Tuple) -> 'View':
+    if not isinstance(self.shape, tuple): raise ValueError(f"expand arg {self.shape=} must be a tuple")
+    if len(new_shape) != len(self.shape): raise ValueError(f"expand arg {new_shape=} must have same number of dimensions as shape {self.shape=}")
+    flat_shape = self.flat_shape
+    if 0 in flat_shape:
+      assert all((s == x == 0) or (s > 0 and (x % s) == 0) for s,x in zip(self.flat_shape, new_shape)), f"can't expand {self.shape} into {new_shape}"
+      return View.create(new_shape)
+    assert isinstance(self.strides, tuple)
+    assert all((s == x or (s == 1 and st == 0)) for s,x,st in zip(flat_shape, new_shape, self.strides)), f"can't expand {self.shape} into {new_shape}"
+    return View.create(new_shape, self.strides)
+
+
+  def reshape(self, shape):
+    assert product(self.shape) == product(shape), f"Cannot reshape {self.shape} to {shape}"
+    return composition(self, View.create(shape))
+
+  @functools.lru_cache(maxsize=None)  # pylint: disable=method-cache-max-size-none
+  def coalesce(self):
+    result_shape  = deque([1])
+    result_strides = deque([0])
+    for (shape,strides) in zip(reversed(flatten(self.shape)),reversed(flatten(self.strides))):
+      if shape == 1:
+        continue
+      elif result_shape[0] == 1:
+        result_shape[0]  = shape
+        result_strides[0] = strides
+      elif result_shape[0] * result_strides[0] == strides:
+        result_shape[0] = result_shape[0] * shape
+      else:
+        result_shape.appendleft(shape)
+        result_strides.appendleft(strides)
+
+    if len(result_shape) == 1:
+      return View.create(result_shape[0], result_strides[0])
+    else:
+      return View.create(tuple(result_shape), tuple(result_strides))
 
   @property
+  @functools.lru_cache(maxsize=None)  # pylint: disable=method-cache-max-size-none
   def continuous(self) -> bool:
-    v = coalesce(self)
-    return is_continuous(v.shape, v.strides)
-
-  @staticmethod
-  @functools.lru_cache(maxsize=None)
-  def create(shape:Tuple, strides:Optional[Tuple]=None):
-    return View(shape, strides)
+    v = self.coalesce()
+    return v.strides == canonicalize_strides(v.shape, compact_strides(v.shape))
 
   def render(self):
+    idxs: Union[Variable, Tuple[Variable, ...]]
     if isinstance(self.shape, tuple):
         idxs = tuple(Variable(f"idx{i}", 0, product(s)-1) for i,s in enumerate(self.shape))
     else:
-        idxs = (Variable("idx", 0, self.shape-1),)
-    return self(idxs).render()
+        idxs = Variable("idx", 0, self.shape-1)
+    return cast(Variable, self(idxs)).render()
 
   def __str__(self):
     return f"View(shape={self.shape}, strides={self.strides}, continuous={self.continuous})"
@@ -71,44 +107,19 @@ class View:
   def __repr__(self):
     return f"View(shape={self.shape}, strides={self.strides}, continuous={self.continuous})"
 
-@functools.lru_cache(maxsize=None)
-def is_continuous(shape: Union[Node, Tuple], strides: Union[Node, Tuple]) -> bool:
-  return strides == compact_strides(shape)
-
 def make_view(*views: View) -> View:
   shape, strides = zip(*((a.shape,a.strides) for a in views))
   return View(shape, strides)
 
 @functools.lru_cache(maxsize=None)
-def coalesce(view):
-  result_shape  = deque([1])
-  result_strides = deque([0])
-  for (shape,strides) in zip(reversed(flatten(view.shape)),reversed(flatten(view.strides))):
-    if shape == 1:
-      continue
-    elif result_shape[0] == 1:
-      result_shape[0]  = shape
-      result_strides[0] = strides
-    elif result_shape[0] * result_strides[0] == strides:
-      result_shape[0] = result_shape[0] * shape
-    else:
-      result_shape.appendleft(shape)
-      result_strides.appendleft(strides)
-
-  if len(result_shape) == 1:
-    return View(result_shape[0], result_strides[0])
-  else:
-    return View(tuple(result_shape), tuple(result_strides))
-
-@functools.lru_cache(maxsize=None)
 def composition(viewA: View, viewB:View):
-  if viewB.strides == 0: return View(viewB.shape, 0)
+  if viewB.strides == 0: return View.create(viewB.shape, 0)
 
   if isinstance(viewB.shape, tuple):
     return make_view(*tuple(composition(viewA, viewB[i]) for i in range(len(viewB.shape))))
   else:
-    result_shape: deque[Union[int, Node, Tuple]] = deque()
-    result_strides: deque[Union[int, Node, Tuple]]  = deque()
+    result_shape: deque[Union[int, Integer, Tuple]] = deque()
+    result_strides: deque[Union[int, Integer, Tuple]]  = deque()
     rest_shape   = viewB.shape
     rest_strides  = viewB.strides
     for (s, d) in zip(reversed(flatten(viewA.shape)[1:]), reversed(flatten(viewA.strides)[1:])):
@@ -121,4 +132,4 @@ def composition(viewA: View, viewB:View):
     result_shape.appendleft(rest_shape)
     result_strides.appendleft(rest_strides * flatten(viewA.strides)[0])
 
-    return coalesce(View(tuple(result_shape), tuple(result_strides)))
+    return View(tuple(result_shape), tuple(result_strides)).coalesce()
